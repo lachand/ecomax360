ac8c57eee389ee225c43465a14f7e826a44f33ab
Lachand-Pascal <valentin@lachand.net>
2025-02-20 13:16:52 +0100

    Add files via upload

---
+++ ecomax360/custom_components/ecomax360/__init__.py
@@ -0,0 +1,13 @@
+import logging
+from homeassistant.helpers.discovery import load_platform
+
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = "ecomax360"
+
+def setup(hass, config):
+    """Configuration de l'intégration."""
+    _LOGGER.info("Démarrage de EcoMax360")
+    load_platform(hass, "sensor", DOMAIN, {}, config)
+    load_platform(hass, "climate", DOMAIN, {}, config)
+    load_platform(hass, "switch", DOMAIN, {}, config)
+    return True
