93888685922cdb403e3c829b3e55c1c9bffd6054
Lachand-Pascal <valentin@lachand.net>
2025-02-21 21:38:42 +0100

    Update __init__.py

--- custom_components/ecomax360/__init__.py
+++ custom_components/ecomax360/__init__.py
@@ -7,7 +7,7 @@ DOMAIN = "ecomax360"
 def setup(hass, config):
     """Configuration de l'intégration."""
     _LOGGER.info("Démarrage de EcoMax360")
-    #load_platform(hass, "sensor", DOMAIN, {}, config)
-    load_platform(hass, "climate", DOMAIN, {}, config)
+    load_platform(hass, "sensor", DOMAIN, {}, config)
+    #load_platform(hass, "climate", DOMAIN, {}, config)
     #load_platform(hass, "switch", DOMAIN, {}, config)
     return True
