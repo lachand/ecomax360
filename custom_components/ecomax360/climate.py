69649140b5a4b177f16a3da299d862bdd31b98f2
Lachand-Pascal <valentin@lachand.net>
2025-03-10 16:53:15 +0100

    Update climate.py

--- custom_components/ecomax360/climate.py
+++ custom_components/ecomax360/climate.py
@@ -107,7 +107,7 @@ class EcomaxThermostat(ClimateEntity):
         mode_code = "011e01"
         code = next((key for key, value in EM_TO_HA_MODES.items() if value == preset_mode), "00")
         trame = Trame("6400", "0100", "29", "a9", mode_code, code).build()
-        await api.send_trame(trame, "a9")
+        await self.api.send_trame(trame, "a9")
 
         await self.async_update()
         self.async_write_ha_state()
@@ -121,14 +121,14 @@ class EcomaxThermostat(ClimateEntity):
         code = "012001" if self._preset_mode in ["SCHEDULE", PRESET_ECO] and self.auto == 1 else "012101"
         trame = Trame("6400", "0100", "29", "a9", code, struct.pack('<f', temperature).hex()).build()
 
-        await api.send_trame(trame, "a9")
+        await self.api.send_trame(trame, "a9")
 
         await self.async_update()
         self.async_write_ha_state()
 
     async def async_update(self):
         trame = Trame("64 00", "20 00", "40", "c0", "647800", "").build()
-        thermostat_data = await api.request(trame, THERMOSTAT, "265535445525f78343", "c0") or {}
+        thermostat_data = await self.api.request(trame, THERMOSTAT, "265535445525f78343", "c0") or {}
         
         if 5 < thermostat_data.get("ACTUELLE", 0) < 35:
             self._target_temperature = thermostat_data["ACTUELLE"]
