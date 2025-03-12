c5b7e5617dc6de19701beb3174ea54bfda9b5dae
Lachand-Pascal <valentin@lachand.net>
2025-03-12 20:51:26 +0100

    Update climate.py

--- custom_components/ecomax360/climate.py
+++ custom_components/ecomax360/climate.py
@@ -140,7 +140,7 @@ class EcomaxThermostat(ClimateEntity):
         self._target_temperature = temperature
         _LOGGER.info("preset: %s", self._preset_mode)
         _LOGGER.info("auto : %s", self.auto)
-        code = "012001" if self._preset_mode in ["Calendrier", PRESET_ECO] and self.auto == 0 else "012101"
+        code = "012001" if (self._preset_mode in ["Calendrier"] and self.auto == 1) or (self._preset_mode in [PRESET_COMFORT]) else "012101"
         trame = Trame("6400", "0100", "29", "a9", code, struct.pack('<f', temperature).hex()).build()
 
         comm = Communication()
