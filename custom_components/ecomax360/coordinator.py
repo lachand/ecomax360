0cf12fdccf829c0af62cf0a55aab10304a5d69de
Lachand-Pascal <valentin@lachand.net>
2025-03-13 15:58:28 +0100

    Update coordinator.py

--- custom_components/ecomax360/coordinator.py
+++ custom_components/ecomax360/coordinator.py
@@ -15,15 +15,15 @@ class EcomaxCoordinator(DataUpdateCoordinator):
             hass,
             _LOGGER,  # Utilisation correcte du logger
             name="EcomaxCoordinator",
-            update_interval=timedelta(seconds=30),  # Mise à jour toutes les 30 secondes
+            update_interval=timedelta(seconds=120),  # Mise à jour toutes les 30 secondes
         )
 
     async def _async_update_data(self):
         """Effectue une seule requête et met à jour toutes les valeurs."""
         try:
-            self._comm.connect()
-            data = axait self._comm.listenFrame("GET_DATAS") or {}
-            self._comm.close()
+            await self._comm.connect()
+            data = await self._comm.listenFrame("GET_DATAS") or {}
+            await self._comm.close()
             return data
         except Exception as err:
             _LOGGER.error(f"Erreur lors de la récupération des données : {err}")
